49b1b62d34519041bb5506fdfcbf1b0f044a2664
Muigai Ernest <ernestmbogo7@gmail.com>
2025-09-17 22:08:15 +0300

    javascript implementation

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const generateBtn = document.getElementById("generate-btn");
 const paletteContainer = document.querySelector(".palette-container");
 
 generateBtn.addEventListener("click", generatePalette);
+
+function generatePalette() {
+   const colors=[]
+   for(let i = 0; i<5; i++) {
+    colors.push(generateRandomColor())
+   }
+}
+function generateRandomColor() {
+    const letters ="0123456789ABCDEF";
+    let color = "#";
+    for(let i = 0; i<6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+}
+generatePalette()
